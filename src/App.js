17675b35fa8a33181dde2d82e66016bd77dd2582
ukozazenje <mzecevic04@gmail.com>
2018-06-17 21:50:06 +0200

    testting git pages

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h3>New movie db</h3>
         <input onChange={this.fetchMovies} />
         <h3>{massage}</h3>
         {this.moviesList(movies)}
